479b9e7abb846209c225033ae991c5f591ae502e
Szieberth Ádám <sziebadam@gmail.com>
2017-01-14 18:29:09 +0100

    InstructionSorter done & Instruction __new__ + __init__

--- si/communication/bits/instruction.py
+++ si/communication/bits/instruction.py
@@ -3,7 +3,8 @@ import types
 import typing
 
 
-from si.helper import classproperty, coroutine
+from si.common.bits.crc import Crc
+from si.helper import classproperty
 from .. import Protocol
 from ..helper import get_protocol_from_cmd
 from . import Cmd, ProtoChar
@@ -25,7 +26,7 @@ class InstructionParts(typing.NamedTuple):
   CMD: typing.Union[bytes, bytearray]
   LEN: typing.Union[bytes, bytearray]
   DATA: typing.Union[bytes, bytearray]
-  CRC: typing.Union[bytes, bytearray]
+  CRC: typing.Union[Crc, bytes, bytearray]
   ETX: typing.Union[bytes, bytearray]
 
 
@@ -33,38 +34,51 @@ class Instruction(bytes):
   CMD = NotImplemented
   TYPE = NotImplemented
 
-  _members = {}
-
   def __init_subclass__(cls, **kwargs):
     super().__init_subclass__(**kwargs)
     if cls.CMD != NotImplemented:
       cls._members[cls.CMD] = cls
 
+  def __new__(cls, arg, check_crc=True, _parts=None):
+    #TODO: __new__ should return member with same CMD when
+    #   _members
+    return super().__new__(cls, arg)
+
+  def __init__(self, arg, check_crc=True, _parts=None):
+    # TODO: sophisticate
+    super().__init__()
+    if _parts is None:
+      try:
+        fact = InstructionSorter(self)
+      except StopIteration as e:
+        _parts, extra_buffer = e.value
+        if extra_buffer:
+          raise ValueError('too much bytes') from None
+      else:
+        raise ValueError('not enough bytes')
+    self._parts = _parts
+    if check_crc:
+      # TODO: document that Crc instance expected
+      check_result = self._parts.CRC.check_bytes(
+          b''.join(self._parts[2:5]))
+      if not check_result:
+        raise ValueError('crc check failed')
+
+  #TODO: from_parts classmethod
+
   @classproperty
   def members(cls):
     return types.MappingProxyType(cls._members)
 
-  @classmethod
-  def consumer_factory(cls) -> 'coroutine function':
-    @coroutine
-    def consumer(cls
-      ) -> StopIteration(InstructionConsumerResult):
-      state = 'start'
-      while True:
-        data = (yield)
-    # TODO: this is an example oroutine function;
-    #   should be replaced with InstructionConsumer (see below)
-    return consumer
+  @property
+  def parts(self):
+      return self._parts
 
-  @classmethod
   def protocol(self) -> Protocol:
     """
     Return si.communication.Protocol based on the command byte
     """
-    if cls.CMD == NotImplemented:
-      return Protocol.NotSet
-    else:
-      return get_protocol_from_cmd(cls.CMD)
+    return get_protocol_from_cmd(self._parts.CMD[0])
 
 
 class Command(Instruction):
@@ -77,13 +91,12 @@ class Response(Instruction):
   _members = {}
 
 
-
-class InstructionConsumerResult(typing.NamedTuple):
-  instruction: Instruction
+class InstructionSorterResult(typing.NamedTuple):
+  instructionparts: InstructionParts
   remaining_bytes: bytes
 
 
-class InstructionFactory:
+class InstructionSorter:
 
   def __init__(self, data=None):
     self.reset()
@@ -106,30 +119,21 @@ class InstructionFactory:
   def parts(self):
       return self._parts
 
-  @coroutine
-  def _coro(self):
-    while True:
-      bytes_ = yield
-      self.buffer.extend(bytes_)
-      if bytes_ == b'\x00':
-        return 'Hello'
-
   def reset(self, clear_buffer=True):
     self.state = InstructionPart.FF
     self.protocol = Protocol.NotSet
+    self.dle = False
     if clear_buffer:
       self._parts = InstructionParts(
           *(bytearray() for _ in InstructionParts._fields))
       self._extra_buffer = bytearray()
-    self.coro=self._coro()
 
   def send(self, bytes_):
     if bytes_:
       state_name = self.state.name.lower()
       method_name = 'send_at_{}'.format(state_name)
       method = getattr(self, method_name)
-      return method(bytes_)
-    #self.coro.send(bytes_)
+      method(bytes_)
 
   def send_at_ff(self, bytes_):
     b = bytes_[:1]
@@ -147,8 +151,9 @@ class InstructionFactory:
       self[InstructionPart.STX].extend(b)
       self.send(bytes_[1:])
     elif not self[InstructionPart.STX]:
-      raise ValueError(('invalid protocol byte '
-          'received at STX state: {:0>2X}').format(b[0]))
+      self._extra_buffer.extend(bytes_)
+      raise ValueError(('invalid protocol byte received at STX '
+          'state: {:0>2X}').format(b[0]))
     else:
       self.state = InstructionPart.CMD
       self.send(bytes_)
@@ -158,8 +163,9 @@ class InstructionFactory:
     try:
       cmd = Cmd(b)
     except ValueError:
-      raise ValueError(('invalid protocol byte '
-          'received at CMD state: {:0>2X}').format(b[0]))
+      self._extra_buffer.extend(bytes_)
+      raise ValueError(('invalid protocol byte received at CMD '
+          'state: {:0>2X}').format(b[0]))
     self[InstructionPart.CMD].extend(b)
     self.protocol = get_protocol_from_cmd(cmd)
     if self.protocol == Protocol.Legacy:
@@ -168,7 +174,65 @@ class InstructionFactory:
       self.state = InstructionPart.LEN
     self.send(bytes_[1:])
 
+  def send_at_len(self, bytes_):
+    b = bytes_[:1]
+    self[InstructionPart.LEN].extend(b)
+    self.state = InstructionPart.DATA
+    self.send(bytes_[1:])
 
-  # TODO: Implement InstructionConsumer with coroutine API;
-  #   goal is to provide intermediate data in a bytearray at any
-  #   time
+  def send_at_data(self, bytes_):
+    if self[InstructionPart.LEN]:
+      exp_len = self[InstructionPart.LEN][0]
+      curr_len = len(self[InstructionPart.DATA])
+      num_bytes_needed = max(0, exp_len - curr_len)
+      num_bytes_to_data = min(len(bytes_), num_bytes_needed)
+      data_bytes = bytes_[:num_bytes_to_data]
+      remaining_bytes = bytes_[num_bytes_to_data:]
+      if num_bytes_needed == num_bytes_to_data:
+        self.state = InstructionPart.CRC
+      self[InstructionPart.DATA].extend(data_bytes)
+      self.send(remaining_bytes)
+    else:
+      b = bytes_[:1]
+      if (not self.dle and b <= b'\x1F'
+          and b != ProtoChar.DLE.value):
+        self.state = InstructionPart.ETX
+        self.send(bytes_)
+      else:
+        if not self.dle and b == ProtoChar.DLE.value:
+          self.dle = True
+        elif self.dle and b >  b'\x1F':
+          self._extra_buffer.extend(bytes_)
+          raise ValueError(('invalid protocol byte received at '
+              'DATA state after DLE: {:0>2X}').format(b[0]))
+        else:
+          self.dle = False
+        self[InstructionPart.DATA].extend(b)
+        self.send(bytes_[1:])
+
+  def send_at_crc(self, bytes_):
+    exp_len = 2 # TODO: XOR on 1 byte?
+    curr_len = len(self[InstructionPart.CRC])
+    num_bytes_needed = max(0, exp_len - curr_len)
+    num_bytes_to_data = min(len(bytes_), num_bytes_needed)
+    data_bytes = bytes_[:num_bytes_to_data]
+    remaining_bytes = bytes_[num_bytes_to_data:]
+    if num_bytes_needed == num_bytes_to_data:
+      self.state = InstructionPart.ETX
+    self[InstructionPart.CRC].extend(data_bytes)
+    self.send(remaining_bytes)
+
+  def send_at_etx(self, bytes_):
+    b = bytes_[:1]
+    if b != ProtoChar.ETX.value:
+      self._extra_buffer.extend(bytes_)
+      raise ValueError(('invalid protocol byte received at ETX '
+          'state: {:0>2X}').format(b[0]))
+    self[InstructionPart.ETX].extend(b)
+    self._extra_buffer.extend(bytes_[1:])
+    parts = InstructionParts(*((bytes(p) if i != 5 else Crc(p))
+        for i, p in enumerate(self._parts)))
+    extra_buffer = bytes(self._extra_buffer)
+    self.reset()
+    raise StopIteration(InstructionSorterResult(parts,
+        extra_buffer))
--- si/helper.py
+++ si/helper.py
@@ -6,14 +6,3 @@ class classproperty:
         self.f = f
     def __get__(self, obj, owner):
         return self.f(owner)
-
-
-# http://www.dabeaz.com/coroutines/coroutine.py
-# http://stackoverflow.com/a/1073582/2334951
-def coroutine(func):
-    @functools.wraps(func)
-    def start(*args,**kwargs):
-        cr = func(*args,**kwargs)
-        next(cr)
-        return cr
-    return start
