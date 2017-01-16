f5eb8b998fd6561eadd77941092b16907f9dc378
Szieberth Ádám <sziebadam@gmail.com>
2017-01-16 12:51:41 +0100

    format development (2)

--- si/protocol/fmt.py
+++ si/protocol/fmt.py
@@ -12,27 +12,39 @@ Instruction = 'Instruction' / Struct(
   # PCPROG5 p. 6
   'ff' / Optional(Const(b'\xFF')),
   # PCPROG5 pp. 5-6
-  'stx' / ExprAdapter(Const(b'\x02')[1:3],
-    decoder = lambda obj,ctx: b''.join(obj),
-    encoder = lambda obj,ctx: [bytes([b]) for b in obj],
+  'ctrl' / OneOf(Bytes(1), b'\x02\x06\x15'),
+  Probe(),
+  Embedded(
+    # https://github.com/construct/construct/issues/296
+    IfThenElse(
+    this.ctrl == b'\x02',
+    Embedded(Struct(
+      Optional(Const(b'\x02')),
+      # PCPROG5 p. 5
+      'cmd' / Bytes(1),
+      Embedded(IfThenElse(
+        # PCPROG5 p. 5
+        this.cmd >= b'\x80' and this.cmd != b'\xC4',
+        # PCPROG5 p. 5
+        Struct(
+          'len' / Rebuild(Byte, len_(this.data)),
+          Embedded(RawCopy(Bytes(this.len))),
+          'crc' / Checksum(Bytes(2), lambda data: crc529(data),
+              lambda ctx: (
+                  ctx._.cmd
+                  + bytes([ctx.len])
+                  + ctx.data
+              ),
+          ),
+        ),
+        Struct(), # TODO
+      )),
+      # PCPROG5 p. 5
+      'etx' / Const(b'\x03'),
+    )),
+    Struct(),
+    )
   ),
-  Embedded(Struct(
-    # PCPROG5 p. 5
-    'cmd' / RawCopy(Byte),
-    Embedded(IfThenElse(
-      # PCPROG5 p. 5
-      this.cmd.value >= 0x80 & this.cmd.value != 0xc4,
-      # PCPROG5 p. 5
-      Struct(
-        'len' / RawCopy(Byte),
-        'data' / RawCopy(Bytes(this.len.value)),
-        'crc' /Checksum(Bytes(2), lambda data: crc529(data), this._.cmd.data + this.len.data + this.data.data),
-      ),
-      Pass, # TODO
-    ))
-  )),
-  # PCPROG5 p. 5
-  'etx' / Const(b'\x03'),
 )
 
 
@@ -81,5 +93,13 @@ def crc529(bytes_: bytes) -> bytes:
 
 if __name__ == '__main__':
   demo_instr = b'\xff\x02\x02\xE0\x00\xE0\x00\x03'
-  demo_instr = b'\xFF\x02\x83\x83\x08\x46\x00\x0D\x00\x08\x46\x04\x33\x31\x31\x0E\x06\x04\x6F\x21\xFF\xFF\xFF\x02\x06\x00\x1B\x17\x3F\x18\x18\x06\x29\x08\x05\x3E\xFE\x0A\xEB\x0A\xEB\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x92\xBA\x1A\x42\x00\xFF\xFF\xE1\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x0B\x07\x0C\x00\x0D\x5D\x0E\x44\x0F\xEC\x10\x2D\x11\x3B\x12\x73\x13\x23\x14\x3B\x15\x01\x19\x1D\x1A\x1C\x1B\xC7\x1C\x00\x1D\xB0\x21\xB6\x22\x10\x23\xEA\x24\x0A\x25\x00\x26\x11\x2C\x88\x2D\x31\x2E\x0B\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xC4\x28\x03'
+
+  Instruction.parse(b'\xff\x06')
+  Instruction.parse(b'\x15')
+
+  demo_instr = b'\xFF\x02\x02\x83\x83\x08\x46\x00\x0D\x00\x08\x46\x04\x33\x31\x31\x0E\x06\x04\x6F\x21\xFF\xFF\xFF\x02\x06\x00\x1B\x17\x3F\x18\x18\x06\x29\x08\x05\x3E\xFE\x0A\xEB\x0A\xEB\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x92\xBA\x1A\x42\x00\xFF\xFF\xE1\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x0B\x07\x0C\x00\x0D\x5D\x0E\x44\x0F\xEC\x10\x2D\x11\x3B\x12\x73\x13\x23\x14\x3B\x15\x01\x19\x1D\x1A\x1C\x1B\xC7\x1C\x00\x1D\xB0\x21\xB6\x22\x10\x23\xEA\x24\x0A\x25\x00\x26\x11\x2C\x88\x2D\x31\x2E\x0B\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xC4\x28\x03'
   demo_parsed = Instruction.parse(demo_instr)
+  demo_instr2 = Instruction.build({k:v for k,v in demo_parsed.items() if k in ('ctrl', 'cmd', 'data')})
+  demo_parsed2 = Instruction.parse(demo_instr2)
+
+  assert demo_instr == demo_instr2
