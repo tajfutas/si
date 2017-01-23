4dcfbb3c924c82f4df86a2253bd3a6986a992590
Szieberth Ádám <sziebadam@gmail.com>
2017-01-23 19:40:20 +0100

    str2bits()

--- si/protocol/_helper.py
+++ si/protocol/_helper.py
@@ -4,6 +4,69 @@ import string
 import typing
 
 
+BITDIGITS = 'oX'
+
+
+def bitdigits_as_booleans(stream: typing.io,
+    space: str = ' ',
+    bitdigits: str = BITDIGITS,
+    exc_cls: typing.Union[None, typing.Type[Exception]] = None,
+  ) -> typing.Iterator[bool]:
+  """
+  Yield booleans by reading the stream for bit character pairs
+
+  Note that the stream must be seekable and tellable.
+
+  Spaces are ignored between bit values. The space parameter
+  defines the space string (default ' ').
+
+  What is considered a bitdigit could be customized with the
+  bitdigits parameter (default 'oX'). Note that matching is
+  case sensitive.
+
+  >>> s = 'oXXXX XoX hello world'
+  >>> stream = io.StringIO(s)
+  >>> gen = bitdigits_as_booleans(stream)
+  >>> li = list(gen)
+  >>> li
+  [False, True, True, True, True, True, False, True]
+  >>> stream.read()
+  ' hello world'
+
+  By default the iteration stops at invalid characters without
+  raising an exception. This can be overridden with an explicit
+  exc_cls (exception class) parameter (defalut StopIteration).
+
+  >>> stream.seek(0)
+  0
+  >>> gen = bitdigits_as_booleans(stream, exc_cls=ValueError)
+  >>> li = list(gen)
+  Traceback (most recent call last):
+    ...
+  ValueError: expected a bitdigit (oX): 'h'
+  >>> stream.read()
+  'oXXXX XoX hello world'
+
+  """
+  exc_cls = StopIteration if exc_cls is None else exc_cls
+  c = None
+  full_fb = fb = stream.tell()
+  while True:
+    if c != space:
+      fb = stream.tell()
+    c = stream.read(1)
+    if not c:
+      break
+    if c == space:
+      continue
+    elif c not in bitdigits:
+      stream.seek((fb if exc_cls == StopIteration else full_fb))
+      efs = 'expected a bitdigit ({}): {!r}'
+      raise exc_cls(efs.format(bitdigits, c))
+    else:
+      yield bool(int(bitdigits.index(c)))
+
+
 def bits(bytes_: bytes,
     reversed_bytes: bool = False,
     reversed_bits: bool = False
@@ -109,7 +172,7 @@ def bytes2str(bytes_:bytes,
 def hexdigits_as_integers(stream: typing.io,
     space: str = ' ',
     exc_cls: typing.Union[None, typing.Type[Exception]] = None,
-  ) -> typing.Iterator[str]:
+  ) -> typing.Iterator[int]:
   """
   Yield integers by reading the stream for hexadecimal character
   pairs
@@ -123,6 +186,7 @@ def hexdigits_as_integers(stream: typing.io,
   >>> stream = io.StringIO(s)
   >>> gen = hexdigits_as_integers(stream)
   >>> li = list(gen)
+  >>> li
   [104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]
   >>> bytes(li)
   b'hello world'
@@ -139,7 +203,7 @@ def hexdigits_as_integers(stream: typing.io,
   >>> li = list(gen)
   Traceback (most recent call last):
     ...
-  ValueError: expected a hexdigit: 'w'
+  ValueError: expected a hexdigit (0123456789abcdefABCDEF): 'w'
   >>> stream.read()
   '68 65 6C 6C6F 2077 6F 72 6C 64 whatever'
   """
@@ -151,13 +215,13 @@ def hexdigits_as_integers(stream: typing.io,
       fb = stream.tell()
     c = stream.read(1)
     if not c:
-      continue
+      continue  # exits the loop without break
     if c == space:
       continue
     elif c not in string.hexdigits:
       stream.seek((fb if exc_cls == StopIteration else full_fb))
-      efs = 'expected a hexdigit: {!r}'
-      raise exc_cls(efs.format(c))
+      efs = 'expected a hexdigit ({}): {!r}'
+      raise exc_cls(efs.format(string.hexdigits, c))
     elif not first_c:
       first_c = c
       continue
@@ -173,12 +237,54 @@ def hexdigits_as_integers(stream: typing.io,
 
 def str2bits(s: str,
     octets: typing.Union[int, None] = None,
-    space: str = None,
-    chars: typing.Sequence[str] = 'oX',
+    space: str = ' ',
+    bitdigits: str = BITDIGITS,
     from_left: bool = False,
-  ) -> typing.Iterator[int]:
-  pass
-  # TODO
+  ) -> bytes:
+  """
+  """
+  # TODO: docstring
+  if not hasattr(s, 'read'):
+    s = io.StringIO(s)
+    exc_cls = ValueError
+  else:
+    exc_cls = None
+  full_fb = s.tell()
+  bools = list(bitdigits_as_booleans(s, space=space,
+      bitdigits=bitdigits, exc_cls=exc_cls))
+  i = 0
+  num_bytes, num_bits = None, None
+  if octets:
+    rem = len(bools) % octets
+    if rem:
+      s.seek(full_fb)
+      efs = ('expected zero as number of booleans modulo '
+          'modulo octets: {} mod {} == {} != 0')
+      raise ValueError(efs.format(len(bools), octets, rem))
+  else:
+    octets = len(bools)
+  result = []
+  num_bytes, num_bits = 0, 0
+  while bools:
+    if num_bytes == 0 and num_bits == 0:
+      num_bytes, num_bits = divmod(octets, 8)
+    if ((from_left and num_bytes)
+        or (not from_left and not num_bits and num_bytes)):
+      bits, bools = bools[:8], bools[8:]
+      num_bytes -= 1
+    elif ((not from_left and num_bits)
+        or (from_left and not num_bytes and num_bits)):
+      bits, bools = bools[:num_bits], bools[num_bits:]
+      num_bits = 0
+    missing_bits = [False] * (8 - len(bits))
+    if missing_bits:
+      if from_left:
+        bits = bits + missing_bits
+      else:
+        bits = missing_bits + bits
+    result.append(sum(b*2**(7-i) for i, b in enumerate(bits)))
+  return bytes(result)
+
 
 def str2bytes(s: typing.Union[str, typing.io],
     space: str = ' ',
@@ -224,4 +330,3 @@ def str2bytes(s: typing.Union[str, typing.io],
     exc_cls = None
   return bytes(x for x in hexdigits_as_integers(s, space=space,
       exc_cls=exc_cls))
-
